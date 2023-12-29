74969e3eb8d3956476e37daab7ff43ea3a261951
Barış Keser <baris.keser@appcent.mobi>
2023-12-29 12:01:26 +0300

    SatellitesApp created

---
+++ app/src/main/java/com/baris/satelliteinfo/SatellitesApp.kt
@@ -0,0 +1,11 @@
+package com.baris.satelliteinfo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created on 29.12.2023.
+ * @author Barış Keser
+ */
+@HiltAndroidApp
+class SatellitesApp: Application()
