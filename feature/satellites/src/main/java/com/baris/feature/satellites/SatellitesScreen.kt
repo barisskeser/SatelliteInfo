975fcc33c0adbcb3a193abe1dafc4935faf2e15d
Barış Keser <baris.keser@appcent.mobi>
2023-12-29 12:19:07 +0300

    SatellitesScreen and SatellitesViewModel created

---
+++ feature/satellites/src/main/java/com/baris/feature/satellites/SatellitesScreen.kt
@@ -0,0 +1,15 @@
+package com.baris.feature.satellites
+
+import androidx.compose.runtime.Composable
+
+/**
+ * Created on 29.12.2023.
+ * @author Barış Keser
+ */
+
+@Composable
+fun SatellitesScreen(
+    viewModel: SatellitesViewModel
+) {
+
+}
---
+++ feature/satellites/src/main/java/com/baris/feature/satellites/SatellitesViewModel.kt
@@ -0,0 +1,13 @@
+package com.baris.feature.satellites
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ * Created on 29.12.2023.
+ * @author Barış Keser
+ */
+@HiltViewModel
+class SatellitesViewModel @Inject constructor() : ViewModel() {
+}
