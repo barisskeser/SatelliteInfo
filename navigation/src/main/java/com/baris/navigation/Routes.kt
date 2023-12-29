1422f2c41eb8dcdf3e11e7bdaf5e0e73cb02799f
Barış Keser <baris.keser@appcent.mobi>
2023-12-29 11:59:01 +0300

    Routes object created on navigation module

---
+++ navigation/src/main/java/com/baris/navigation/Routes.kt
@@ -0,0 +1,10 @@
+package com.baris.navigation
+
+/**
+ * Created on 29.12.2023.
+ * @author Barış Keser
+ */
+object Routes {
+    const val SATELLITES = "satellites"
+    const val SATELLITE_DETAIL = "satelliteDetail"
+}
