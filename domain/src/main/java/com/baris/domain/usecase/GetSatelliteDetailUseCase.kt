5ace222c54e27946e35ff8004447db56def6461b
Barış Keser <baris.keser@appcent.mobi>
2023-12-30 13:29:53 +0300

    GetSatelliteDetailUseCase created

---
+++ domain/src/main/java/com/baris/domain/usecase/GetSatelliteDetailUseCase.kt
@@ -0,0 +1,27 @@
+package com.baris.domain.usecase
+
+import com.baris.common.Constants
+import com.baris.core.IResult
+import com.baris.core.ResultError
+import com.baris.domain.model.SatelliteDetail
+import com.baris.domain.repository.SatelliteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+/**
+ * Created on 30.12.2023.
+ * @author Barış Keser
+ */
+class GetSatelliteDetailUseCase @Inject constructor(
+    private val repository: SatelliteRepository
+) {
+    operator fun invoke(id: Int): Flow<IResult<SatelliteDetail?>> = flow {
+        emit(IResult.Loading)
+        val satellites = repository.getSatelliteDetail(id)
+        emit(IResult.Success(satellites))
+    }.catch {
+        emit(IResult.Error(ResultError(it.localizedMessage ?: Constants.DEFAULT_ERROR_MESSAGE, it)))
+    }
+}
