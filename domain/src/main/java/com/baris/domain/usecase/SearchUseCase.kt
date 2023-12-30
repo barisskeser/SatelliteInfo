93b454b5277cdba02c9b6480e4da3f476c0771b9
Barış Keser <baris.keser@appcent.mobi>
2023-12-30 13:30:03 +0300

    SearchUseCase created

---
+++ domain/src/main/java/com/baris/domain/usecase/SearchUseCase.kt
@@ -0,0 +1,27 @@
+package com.baris.domain.usecase
+
+import com.baris.common.Constants
+import com.baris.core.IResult
+import com.baris.core.ResultError
+import com.baris.domain.model.Satellite
+import com.baris.domain.repository.SatelliteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+/**
+ * Created on 30.12.2023.
+ * @author Barış Keser
+ */
+class SearchUseCase @Inject constructor(
+    private val repository: SatelliteRepository
+) {
+    operator fun invoke(query: String): Flow<IResult<List<Satellite>>> = flow {
+        emit(IResult.Loading)
+        val satellites = repository.search(query)
+        emit(IResult.Success(satellites))
+    }.catch {
+        emit(IResult.Error(ResultError(it.localizedMessage ?: Constants.DEFAULT_ERROR_MESSAGE, it)))
+    }
+}
