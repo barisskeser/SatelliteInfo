204d0190a98693a6643582618636d3771972e3da
Barış Keser <baris.keser@appcent.mobi>
2023-12-30 15:55:35 +0300

    functions has set as inline

--- core/src/main/java/com/baris/core/extensions/IResultExt.kt
+++ core/src/main/java/com/baris/core/extensions/IResultExt.kt
@@ -9,17 +9,17 @@ import com.baris.core.ResultError
  * @author Barış Keser
  */
 
-fun <T> IResult<T>.onLoading(body: () -> Unit) = this.also {
+inline fun <T> IResult<T>.onLoading(body: () -> Unit) = this.also {
     if (this is IResult.Loading)
         body.invoke()
 }
 
-fun <T> IResult<T>.onSuccess(body: (T) -> Unit) = this.also {
+inline fun <T> IResult<T>.onSuccess(body: (T) -> Unit) = this.also {
     if (this is IResult.Success)
         body.invoke(this.data)
 }
 
-fun <T> IResult<T>.onError(body: (ResultError) -> Unit) = this.also {
+inline fun <T> IResult<T>.onError(body: (ResultError) -> Unit) = this.also {
     if (this is IResult.Error)
         body.invoke(this.error)
 }
