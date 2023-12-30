0f90c2c770d95506611b1afa62ff9feb819d10ae
Barış Keser <baris.keser@appcent.mobi>
2023-12-30 13:29:43 +0300

    GetSatelliteDetailUseCase created

--- core/src/main/java/com/baris/core/IResult.kt
+++ core/src/main/java/com/baris/core/IResult.kt
@@ -6,8 +6,8 @@ import androidx.annotation.Nullable
  * Created on 29.12.2023.
  * @author Barış Keser
  */
-sealed interface IResult<T> {
-    data object Loading: IResult<Nullable>
+sealed interface IResult<out T> {
+    data object Loading: IResult<Nothing>
     data class Success<T>(val data: T): IResult<T>
     data class Error<T>(val error: ResultError): IResult<T>
 }
---
+++ domain/src/main/java/com/baris/domain/usecase/GetSatelliteListUseCase.kt
@@ -0,0 +1,28 @@
+package com.baris.domain.usecase
+
+import com.baris.common.Constants
+import com.baris.core.IResult
+import com.baris.core.ResultError
+import com.baris.domain.model.Satellite
+import com.baris.domain.repository.SatelliteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+/**
+ * Created on 30.12.2023.
+ * @author Barış Keser
+ */
+class GetSatelliteListUseCase @Inject constructor(
+    private val repository: SatelliteRepository
+) {
+    operator fun invoke(): Flow<IResult<List<Satellite>>> = flow {
+        emit(IResult.Loading)
+        val satellites = repository.getSatelliteList()
+        emit(IResult.Success(satellites))
+    }.catch {
+        emit(IResult.Error(ResultError(it.localizedMessage ?: Constants.DEFAULT_ERROR_MESSAGE, it)))
+    }
+
+}
