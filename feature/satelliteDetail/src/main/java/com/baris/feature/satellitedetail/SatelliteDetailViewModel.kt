b60e8442216a225eeddbd54bfd41a9b5ee686058
Barış Keser <baris.keser@appcent.mobi>
2023-12-31 14:39:54 +0300

    @HiltViewModel annotation added to SatelliteDetailViewModel

--- feature/satelliteDetail/src/main/java/com/baris/feature/satellitedetail/SatelliteDetailViewModel.kt
+++ feature/satelliteDetail/src/main/java/com/baris/feature/satellitedetail/SatelliteDetailViewModel.kt
@@ -1,13 +1,13 @@
 package com.baris.feature.satellitedetail
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 /**
  * Created on 29.12.2023.
  * @author Barış Keser
  */
-@HiltAndroidApp
+@HiltViewModel
 class SatelliteDetailViewModel @Inject constructor() : ViewModel() {
 }
